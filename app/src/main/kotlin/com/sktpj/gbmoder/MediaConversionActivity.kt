fdaf9b4191223967cc068a555813798b71fb4887
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-08-21 17:12:10 +0900

    Localize media conversion UI

--- app/src/main/kotlin/com/sktpj/gbmoder/MediaConversionActivity.kt
+++ app/src/main/kotlin/com/sktpj/gbmoder/MediaConversionActivity.kt
@@ -40,7 +40,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
